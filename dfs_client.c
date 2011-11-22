2d4b3a157fcf9eb56cd1d1a94b9fad5886898f61
mayur <mayur@mayur-ThinkPad-T420.(none)>
2011-11-22 06:30:46 -0500

    first commit on work

---
+++ dfs_client.c
@@ -0,0 +1,40 @@
+#include<fuse.h>
+#include<stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+static int dfsClient_open(const char *path, struct fuse_file_info *fi)
+{
+	printf("Hello World\n");
+	return 0;
+}
+
+static int dfsClient_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+			 off_t offset, struct fuse_file_info *fi)
+{
+	printf("Hello World\n");
+/*	printf("Hello World\n");
+	(void) offset;
+	(void) fi;
+
+	if (strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	filler(buf, NULL, NULL, 0);
+*/
+	return 0;
+}
+
+static struct fuse_operations dfsClient_oper = {
+	.getdir		= (void*)dfsClient_readdir,
+	.open		= (void*)dfsClient_open,
+};
+
+int main(int argc, char *argv[])
+{
+	int i=umask(0);
+	printf("%x\n",i);
+	return fuse_main(argc, argv, &dfsClient_oper);	
+}
